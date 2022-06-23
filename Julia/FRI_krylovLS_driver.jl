1aff5126c19a8cf07574341fee1b88c54be73aff
Jonathan Weare <6777305+jonathanweare@users.noreply.github.com>
2022-06-22 22:18:43 -0400

    testing

--- Julia/FRI_krylovLS_driver.jl
+++ Julia/FRI_krylovLS_driver.jl
@@ -18,10 +18,11 @@ include("compress.jl")
 # end
 
 
-n = 100
-d = 100
+n = 20
+d = 20
+p = 20
 
-m = 100
+m = 20
 
 ee = ones(n-1)
 dd = range(1,length=n)
@@ -32,18 +33,14 @@ A = spdiagm(-1=>ee,0=>dd.*3,1=>ee.*2)
 b = randn(n)
 b = b./norm(b)
 
-B = Array{Float64,2}(undef,n,d+1)
-# S = randn(2*d,n)
+xtrue = A\b
+
 S = I
+# S = randn(p,n)
+B = Array{Float64,2}(undef,n,d)
+SAB = Array{Float64,2}(undef,p,d)
 
-SAB = Array{Float64,2}(undef,n,d)
-
-residual = user_sparse_matvec(b)
-residual = residual.-b
-
-println(norm(residual))
-
-x = copy(residual)
+x = copy(b)
 
 for k=1:d
     global x
@@ -52,25 +49,33 @@ for k=1:d
 
     B[:,k] = copy(x)
 
+    # if k>1
+    #     Q, R = qr(B[:,1:k])
+    #     B[:,1:k] = Matrix(Q)
+    #     x = B[:,k]
+    # end
+
     pivotal_compress(x,m)
     # x = user_sparse_matvec(x)
-
     x = A*x
 
     SAB[:,k] = S*x
-
-    Q, R = qr(B[:,1:k])
-
-    B[:,1:k] = Matrix(Q)
 end
 
-Sr = S*residual
+Sr = S*b
+
+SAB2 = S*A*B[:,1:d]
 
 y = SAB\Sr
 
-x = b.-B[:,1:d]*y
+println(SAB2*y.-Sr)
 
-residual = user_sparse_matvec(x)
-residual = residual.-b
+x = B[:,1:d]*y
+
+println(S*A*x.-Sr)
+
+
+
+residual = A*x.-b
 
 println(norm(residual))
